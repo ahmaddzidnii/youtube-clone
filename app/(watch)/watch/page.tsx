5dcda720447c780f5914a7fb54ce86c09c2efc16
ahmaddzidnii <zdngaming312@gmail.com>
2024-03-11 12:36:52 +0700

    feature: membuat truncate text di kolom komentar

--- app/(watch)/watch/page.tsx
+++ app/(watch)/watch/page.tsx
@@ -9,6 +9,7 @@ import {
 import { VideoPlayer } from "@/components/watch/video-player";
 import Image from "next/image";
 import { BsFilterLeft } from "react-icons/bs";
+import { CommentExpand } from "@/components/watch/comment/comment-expand";
 
 export const metadata = {
   title: "title video",
@@ -102,8 +103,8 @@ export default function WatchPage({
           </div>
           {/* List komentar */}
           <div className="mt-10 flex-col">
-            {Array.from({ length: 8 }).map((_, i) => (
-              <div className="flex gap-x-2" key={i}>
+            {Array.from({ length: 10 }).map((_, i) => (
+              <div className="mt-2 flex gap-x-2" key={i}>
                 <div className="relative aspect-square h-[40px] w-[40px] overflow-hidden rounded-full">
                   <Image
                     alt="profile"
@@ -113,24 +114,17 @@ export default function WatchPage({
                 </div>
                 <div className="flex-1">
                   <div className="flex">
-                    <h2 className="text-[13px] font-semibold">@madzChannel</h2>
+                    <h2 className="me-2 text-[13px] font-semibold">
+                      @madzChannel
+                    </h2>
                     <span className="text-[12px] text-muted-foreground">
                       1 hari yang lalu
                     </span>
                   </div>
-                  <p className="text-[14px]">
-                    It is a long established fact that a reader will be
-                    distracted by the readable content of a page when looking at
-                    its layout. The point of using Lorem Ipsum is that it has a
-                    more-or-less normal distribution of letters, as opposed to
-                    using 'Content here, content here', making it look like
-                    readable English. Many desktop publishing packages and web
-                    page editors now use Lorem Ipsum as their default model
-                    text, and a search for 'lorem ipsum' will uncover many web
-                    sites still in their infancy. Various versions have evolved
-                    over the years, sometimes by accident, sometimes on purpose
-                    (injected humour and the like).
-                  </p>
+                  <CommentExpand
+                    text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora veritatis sunt sequi inventore veniam odio consequatur fuga! Doloribus facere similique assumenda eveniet maxime tempora ex fuga dolore ab non ipsa corrupti pariatur, alias, debitis ratione necessitatibus maiores. Sit esse molestiae id rerum fugit nostrum illo laudantium temporibus soluta voluptates ab voluptas perspiciatis voluptatem, sequi, tempore debitis assumenda sunt aspernatur incidunt. Iste, aperiam perferendis nostrum nobis eligendi sit alias odit et exercitationem excepturi voluptate, asperiores corporis, ut molestiae cupiditate odio? Aspernatur laboriosam dolor dolores ipsa eaque eum totam commodi quae, voluptates fugit est sint assumenda ratione molestias distinctio sapiente animi nulla quod temporibus! Sed, magnam. Impedit perferendis qui itaque quas in aliquid quasi delectus, tempore maiores quaerat magnam officia quam non est ratione. Quia natus vero quibusdam assumenda quod corporis nostrum sequi dolor accusamus similique repellendus, iure iusto voluptatum perspiciatis ipsam, fugiat possimus maxime quidem amet. Quasi molestias tempora cupiditate, alias ducimus tempore odio nobis ipsum rem aliquam laborum voluptatem architecto totam facilis quae perspiciatis distinctio, labore, optio nihil sit nostrum? Expedita velit cumque, aliquam sequi laborum ad nostrum inventore ducimus eius? Labore, ducimus? Adipisci deleniti reiciendis quibusdam, dicta quam, recusandae omnis cumque amet a tempora quae temporibus! Praesentium autem ex suscipit! Temporibus, amet debitis dignissimos ipsum accusamus reprehenderit eaque? Saepe placeat omnis asperiores eligendi harum cum dolor id, unde pariatur laborum ab suscipit quidem vel, temporibus quibusdam maiores veritatis quasi at a totam, alias cumque praesentium. Nesciunt cum voluptatum a dolor voluptatibus vel expedita quia esse tempore sint, harum beatae velit! Quisquam repellat ut a optio consequuntur possimus voluptatem nostrum vitae! Quidem, asperiores adipisci? Placeat odit temporibus eligendi delectus atque illum ad aspernatur voluptas veniam maiores ea aperiam vero voluptatem asperiores unde velit magnam labore eius, ab quaerat consequatur similique! Repellendus quod numquam vero nobis quidem. Repellendus est necessitatibus animi!"
+                    maxLength={400}
+                  />
                   <div className="flex items-center text-[12px]">
                     <Button
                       variant="ghost"
@@ -167,7 +161,7 @@ export default function WatchPage({
       <div className="w-full xl:w-[405px]">
         <div className="flex flex-col gap-3">
           {/* Lebar layar xl ke atas */}
-          {Array.from({ length: 20 }).map((_, i) => (
+          {Array.from({ length: 30 }).map((_, i) => (
             <RecommendationVideoXLToUp key={i} />
           ))}
         </div>
---
+++ components/watch/comment/comment-expand.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
+interface CommentExpandProps {
+  className?: string;
+  text: string;
+  maxLength: number;
+}
+
+export const CommentExpand = ({
+  className,
+  text,
+  maxLength,
+}: CommentExpandProps) => {
+  const [isExpand, setIsExpand] = useState(false);
+
+  const truncated = (text: string, maxLength: number): string => {
+    if (text.length <= maxLength) {
+      return text;
+    }
+    return text.slice(0, maxLength) + "...";
+  };
+
+  const shouldShowExpandButton = text.length > maxLength;
+
+  const textRender = isExpand ? text : truncated(text, maxLength);
+
+  return (
+    <>
+      <p className={cn("overflow-hidden text-[14px]", className)}>
+        {textRender}
+      </p>
+      {shouldShowExpandButton && (
+        <div className="text-[14px] font-medium">
+          {!isExpand ? (
+            <Button
+              variant="link"
+              className="rounded-3xl px-0"
+              onClick={() => setIsExpand(true)}
+            >
+              Baca selengkapnya
+            </Button>
+          ) : (
+            <Button
+              variant="link"
+              className="rounded-3xl px-0"
+              onClick={() => setIsExpand(false)}
+            >
+              Lebih sedikit
+            </Button>
+          )}
+        </div>
+      )}
+    </>
+  );
+};
